ae3ed86b39fac437c0d39edd839371fca49c5ad5
matthiaaas <33065597+matthiaaas@users.noreply.github.com>
2021-08-16 14:04:33 +0200

    add optional routing middleware

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
 import path from "path"
-import type { Express } from "express"
+import { Router } from "express"
 
 import config from "./config"
 
 import { walk, generateRoutes, getHandlers, getMethodKey } from "./utils"
 
-interface IOptions {
-  directory?: string
-  methodExports?: string[]
-}
-
 /**
- * @param app - An express app instance
+ * Routing factory
+ *
+ * ```ts
+ * createRouter(app)
+ * ```
+ *
+ * @param app - An express app or router instance
  * @param opts - An options object
  */
-export default <T>(app: T, opts: IOptions = defaultOptions): T => {
+const createRouter = <T>(app: T, opts: IOptions = defaultOptions): T => {
   const options = { ...defaultOptions, ...opts }
 
   const files = walk(options.directory)
@@ -36,11 +37,32 @@ export default <T>(app: T, opts: IOptions = defaultOptions): T => {
     }
     // wildcard default export route matching
     if (typeof exported.default !== "undefined") {
-      ;(app as unknown as Express).all(url, ...getHandlers(exported.default))
+      ;(app as unknown as Router).all(url, ...getHandlers(exported.default))
     }
   }
 
   return app
+}
+
+export default createRouter
+
+/**
+ * Router middleware
+ *
+ * ```ts
+ * app.use(router())
+ * ```
+ *
+ * @param opts - An options object
+ * @returns Express Router object
+ */
+export const router = (opts: IOptions = defaultOptions) => {
+  return createRouter(Router(), opts)
+}
+
+interface IOptions {
+  directory?: string
+  methodExports?: string[]
 }
 
 const defaultOptions: IOptions = {
